2d2ab69d96d27d8cb9f9224fd8c4b0ed09decc4a
Calda <calstephens98@gmail.com>
2015-04-14 14:39:47 -0400

    First efforts

--- Emoji Playlist/ViewController.swift
+++ Emoji Playlist/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hiddenField: UITextField!
+    @IBOutlet weak var display: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        hiddenField.becomeFirstResponder()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hiddenInputReceived(sender: UITextField, forEvent event: UIEvent) {
+        var emoji = sender.text.substringFromIndex(sender.text.endIndex.predecessor()) as NSString
+        println(emoji)
+        println(emoji.length)
+        if emoji.length % 4 == 0 && emoji.length > 4 { //flags stick together for some reason?
+            emoji = emoji.substringFromIndex(emoji.length - 4)
+        }
+        display.text = emoji as String
+    }
 
 }
-
